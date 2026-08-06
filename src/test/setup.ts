1995bf54c50a194cb95ad6e807a00c185c1ecfac
Lucas Correia <lucascorreianasc1108@gmail.com>
2026-08-06 01:08:22 -0300

    fix: tests build

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { TextEncoder, TextDecoder } from 'util'
+import { TextEncoder, TextDecoder } from 'node:util'
 
 if (!globalThis.TextEncoder) {
   globalThis.TextEncoder = TextEncoder
